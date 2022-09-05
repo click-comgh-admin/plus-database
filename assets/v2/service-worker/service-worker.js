ad436096cedafaa126d23bd3e54b4ea2ce628877
ninjavroom <ninjavroom@gmail.com>
2022-09-05 22:06:41 +0000

    Dev Computer 05/09/2022 07

--- assets/v2/service-worker/service-worker.js
+++ assets/v2/service-worker/service-worker.js
@@ -1,5 +1,5 @@
-const STATIC_CACHE_NAME = 'akwaaba-database-site-base-static-v0.003';
-const DYNAMIC_CACHE_NAME = 'akwaaba-database-site-dynamic-requests-v0.003';
+const STATIC_CACHE_NAME = 'akwaaba-database-site-base-static-v0.004';
+const DYNAMIC_CACHE_NAME = 'akwaaba-database-site-dynamic-requests-v0.004';
 const ASSETS = []; // DYNAMICALLY GENERATED IN PHP VIEW
 const UNCACHEABLE_URLS = []; // DYNAMICALLY GENERATED IN PHP VIEW
 
